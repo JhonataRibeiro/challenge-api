1cd1a1f0618ca1f149a00be0d39534b714b2543e
Jhonata Ribeiro <jhonata.ribeiro.ti@gmail.com>
2017-05-15 22:34:46 -0300

    initial

---
+++ src/main/java/br/com/jhonataribeiro/model/Medico.java
@@ -0,0 +1,165 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package br.com.jhonataribeiro.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+@SuppressWarnings("serial")
+@Entity
+@XmlRootElement
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class Medico implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Pattern(regexp = "[^0-9]*", message = "Must not contain numbers")
+    private String primeiroNome;
+    
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Pattern(regexp = "[^0-9]*", message = "Must not contain numbers")
+    private String ultimoNome;
+
+    @NotNull
+    @NotEmpty
+    @Email
+    private String email;
+    
+    @NotNull
+    @Column
+    private Boolean ativo;
+    
+    
+    @NotNull
+    @Size(min = 1, max = 100)
+    private String cidade;
+    
+    @NotNull
+    @Size(min = 1, max = 100)
+    private String estado;
+    
+    
+    /*TODO: adicionar enum*/
+    @NotNull
+    @Column
+    private String status;
+    
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="id_especialidade",nullable=false)
+	@NotNull
+    private Especialidade especialidade;
+
+    public Long getId() {
+        return id;
+    }
+
+	public String getPrimeiroNome() {
+		return primeiroNome;
+	}
+
+	public void setPrimeiroNome(String primeiroNome) {
+		this.primeiroNome = primeiroNome;
+	}
+
+	public String getUltimoNome() {
+		return ultimoNome;
+	}
+
+	public void setUltimoNome(String ultimoNome) {
+		this.ultimoNome = ultimoNome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Especialidade getEspecialidade() {
+		return especialidade;
+	}
+
+	public void setEspecialidade(Especialidade especialidade) {
+		this.especialidade = especialidade;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+    
+}
---
+++ target/classes/import.sql
@@ -0,0 +1,29 @@
+--
+-- JBoss, Home of Professional Open Source
+-- Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
+-- contributors by the @authors tag. See the copyright.txt in the
+-- distribution for a full listing of individual contributors.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- http://www.apache.org/licenses/LICENSE-2.0
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+-- You can use this file to load seed data into the database using SQL statements
+insert into Member (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212');
+
+insert into Especialidade (id, titulo) values (1,'Cardiologista');
+insert into Especialidade (id, titulo) values (2,'Ginecologita');
+insert into Especialidade (id, titulo) values (3,'Ortopedista');
+insert into Especialidade (id, titulo) values (4,'Neurologista');
+insert into Especialidade (id, titulo) values (5,'Pneumologista');
+insert into Especialidade (id, titulo) values (6,'Psiquiatra');
+
+insert into Medico(id, primeiroNome, ultimoNome, email, ativo, cidade, estado, status, id_especialidade) values (1,'Lucas','Santos da Silba','lucas@gmail.com',true,'Brasilia','DF','ativo',1);
+insert into Medico(id, primeiroNome, ultimoNome, email, ativo, cidade, estado, status, id_especialidade) values (2,'Diogo','Silveira da Costa','diego@gmail.com',true,'Goiania','GO','ativo',2);
